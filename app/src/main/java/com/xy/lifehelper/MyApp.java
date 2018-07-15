56113e78907e2c33b7e8cd81bb0b944ed60cc627
sheajin <SheaJin@126.com>
2018-07-15 22:54:04 +0800

    update config

---
+++ app/src/main/java/com/xy/lifehelper/MyApp.java
@@ -0,0 +1,14 @@
+package com.xy.lifehelper;
+
+import android.app.Application;
+
+/**
+ * Created by jxy on 2018/7/15.
+ */
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
